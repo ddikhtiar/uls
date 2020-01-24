fa806c785c2d3eb4814409be14b65bfe41fa9298
ddikhtiar <dikhtyard@gmail.com>
2020-01-24 18:25:44 +0200

    Test

--- src/main.c
+++ src/main.c
@@ -3,11 +3,16 @@
 static void sx_print_arr(char **arr);
 
 int main (int argc, char **argv) {
-    char **flags = NULL;
-    char **dirs = NULL;
+    char **flags = mx_flags_arr(argv);
+    char **dirs = mx_dir_arr(argv);
 
     if (argc > 1) {
-        
+		mx_printstr("FLAGS:\n");
+        sx_print_arr(flags);
+		mx_printstr("\n========\n");
+		mx_printstr("DIRS:\n");
+		mx_check_arr_dir(&dirs);
+		sx_print_arr(dirs);
         system("leaks uls");
     }
 }
